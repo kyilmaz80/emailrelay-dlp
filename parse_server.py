2cf74f2ee4cd4d14a8f6abaf1466042210ec0185
Koray YILMAZ <kyilmaz80@gmail.com>
2017-07-07 18:34:53 +0300

    Configuration parameters separated from code

--- parse_server.py
+++ parse_server.py
@@ -8,22 +8,16 @@ import sys
 import re
 import logging
 import csv
+import params
 from threading import Thread
 
 """
 EmailRelay eml parse threaded server process
-date 170607
+date 170707
 """
 __author__ = "Koray YILMAZ"
 __email__ = 'kyilmaz80@gmail.com'
-__version__ = "1.13"
-
-# config constants
-HOST = 'parser.test.local'
-PORT = 1235
-MAX_REQUESTS = 10
-PATTERN_FILE = './PATTERNS'
-EML_MOUNT = '/mnt/nfs'
+__version__ = "1.14"
 
 
 class SocketThread(Thread):
@@ -49,7 +43,7 @@ class SocketThread(Thread):
         logger.info("Data: %s" % str(data))
         msg_fpath = str(data)
         msg_fpath = msg_fpath.rstrip()
-        self.msg_path = get_msg_fpath(msg_fpath, EML_MOUNT)
+        self.msg_path = get_msg_fpath(msg_fpath, params.EML_MOUNT)
 
     def send(self):
         ret_code = parse_msg.main(self.msg_path, patterns, regexes)
@@ -91,7 +85,7 @@ logger = logging.getLogger(__name__)
 logger.info('Pattern tree generating only once')
 
 # load patterns
-patterns = load_patterns(PATTERN_FILE)
+patterns = load_patterns(params.PATTERN_FILE)
 patternsList = [t[0] for t in patterns.values()]
 regexes = [re.compile(p) for p in patternsList]
 
@@ -102,17 +96,17 @@ logger.info('Server socket opened...')
 
 # bind to the port
 try:
-    serversocket.bind((HOST, PORT))
-    logger.info('%s:%s bind done', HOST, PORT)
+    serversocket.bind((params.HOST, params.PORT))
+    logger.info('%s:%s bind done', params.HOST, params.PORT)
 except Exception as e:
-    logger.error('%s:%s bind failure!', HOST, PORT)
+    logger.error('%s:%s bind failure!', params.HOST, params.PORT)
     sys.exit(1)
 
 # queue up to 10 requests
-serversocket.listen(MAX_REQUESTS)
+serversocket.listen(params.MAX_REQUESTS)
 while True:
     logger.info('------------------------------------------')
-    logger.info('Server is waiting for data on port: %s', PORT)
+    logger.info('Server is waiting for data on port: %s', params.PORT)
     clientsocket, addr = serversocket.accept()
     logger.info("Got a connection from %s" % str(addr))
     sock_thread = SocketThread("Thread-" + str(addr[1]),
